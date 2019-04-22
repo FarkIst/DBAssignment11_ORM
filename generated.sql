16bd39d0fb1b16b2d106ffedce47aa3f7d81afb9
VladMB <cph-mb402@cphbusiness.dk>
2019-04-22 22:20:31 +0200

    auto increment ids

--- generated.sql
+++ generated.sql
@@ -1,35 +1,33 @@
 CREATE DATABASE IF NOT EXISTS `microshop`;
 
-DROP TABLE IF EXISTS `microshop`;
+USE microshop;
+
+DROP TABLE IF EXISTS `customer`;
 CREATE TABLE `microshop`.`customer` (
-	`id` INT NOT NULL,
-	PRIMARY KEY (`id`),
+	`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	`name` VARCHAR(45) NULL
 );
 
-DROP TABLE IF EXISTS `microshop`;
+DROP TABLE IF EXISTS `order`;
 CREATE TABLE `microshop`.`order` (
-	`id` INT NOT NULL,
-	PRIMARY KEY (`id`),
+	`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	`date` VARCHAR(45) NULL,
 	`total` INT NULL,
 	`customer_id` INT NULL
 );
 
-DROP TABLE IF EXISTS `microshop`;
+DROP TABLE IF EXISTS `orderline`;
 CREATE TABLE `microshop`.`orderline` (
-	`id` INT NOT NULL,
-	PRIMARY KEY (`id`),
+	`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	`order_id` INT NULL,
 	`product_id` INT NULL,
 	`count` INT NULL,
 	`total` INT NULL
 );
 
-DROP TABLE IF EXISTS `microshop`;
+DROP TABLE IF EXISTS `product`;
 CREATE TABLE `microshop`.`product` (
-	`id` INT NOT NULL,
-	PRIMARY KEY (`id`),
+	`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	`name` VARCHAR(45) NULL,
 	`price` INT NULL
 );
